1e31c097ea6918ec17f192a97971505a4f167737
Vishnunarayan K I <appukuttancr@gmail.com>
2018-08-30 16:51:07 +0530

    prettify

--- app/app.py
+++ app/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request, send_file
-import matplotlib.pyplot as plt
 from io import BytesIO
+
+from flask import (Flask,
+                   render_template,
+                   request,
+                   send_file)
+
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -12,23 +17,28 @@ def home():
 
 @app.route('/plot', methods=['POST'])
 def plot():
-    labels = [k for k in request.form.keys() if k.startswith('col-')]
     data = dict(request.form)
-    print(data)
+
+    labels = [k for k in data.keys() if k.startswith('col-')]
 
     try:
-        x = [int(v[0]) for k, v in data.items() if k.endswith('0') and not k.startswith('col')]
-        y = [int(v[0]) for k, v in data.items() if k.endswith('1') and not k.startswith('col')]
+        x = [int(v[0]) for k, v in data.items()
+             if k.endswith('0') and not k.startswith('col')]
+        y = [int(v[0]) for k, v in data.items()
+             if k.endswith('1') and not k.startswith('col')]
         plot_type = data['plot-type'][0]
     except Exception:
         return render_template('error.html')
 
     fig, ax = plt.subplots()
+
     getattr(ax, plot_type)(x, y)
     ax.set(xlabel=labels[0], ylabel=labels[1])
+
     img = BytesIO()
     plt.savefig(img)
     img.seek(0)
+
     return send_file(img, mimetype='image/png')
 
 
